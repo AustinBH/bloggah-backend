03a80bef0690e15c867751da7d5d9d566c24c2cb
AustinBH <harlowaustin2@gmail.com>
2019-10-14 16:36:30 -0700

    updated post serializer to include comments

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :user
+  attributes :id, :title, :content, :user, :comments
 
   def user
     {id: object.user.id, username: object.user.username}
+  end
+  def comments
+    self.object.comments.map do |comment|
+      {id: comment.id, text: comment.text}
+    end
   end
 end
