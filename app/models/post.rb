dccca72ff07ea7039164dc4fbb09bc8bd5c8f9be
AustinBH <harlowaustin2@gmail.com>
2019-10-22 17:53:12 -0700

    added post title + content validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
     has_many :comments
 
     validates :title, length: {maximum: 250}
+    validates :title, presence: true
+    validates :content, presence: true
 end
